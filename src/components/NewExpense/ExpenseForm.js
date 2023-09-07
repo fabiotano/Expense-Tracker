28aaca1a3a99d5a2e05a62c54299704996a27b11
fabiotano <fabio.capriotti20@gmail.com>
2023-09-07 21:59:04 +0200

    updating expenses array in App.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -34,7 +34,7 @@ export default function ExpenseForm(props) {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: parseFloat(enteredAmount),
       date: new Date(enteredDate),
     };
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -7,8 +7,8 @@ const NewExpense = (props) => {
 
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
+      id: Math.random().toString(),
       ...enteredExpenseData,
-      id: Math.random().toString,
     };
     // console.log(expenseData);
     props.onAddExpenseData(expenseData);
